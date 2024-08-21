78a52a8f1a420d234f339b39474d16a8791b44e0
Joe Estrem <estremjoe@gmail.com>
2024-08-21 11:08:41 +0900

    poll progress 3

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -3,10 +3,27 @@ class PlansController < ApplicationController
 
   def new
     @plan = Plan.new
+    @plan.polls.build
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+    if @plan.save
+      redirect_to @plan, notice: 'Plan was successfully created.'
+    else
+      render :new
+    end
   end
 
   def show
     @plan = Plan.find(params[:id])
   end
 
+
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:date_time, :location, polls_attributes: [:mood, :alcohol, :smoking, :food])
+  end
 end
--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,8 +1,26 @@
 class PollsController < ApplicationController
   skip_before_action :authenticate_user!
 
+  def new
+    @poll = Poll.new
+  end
+
   def show
     @poll = Poll.find(params[:id])
   end
 
+  def create
+    @poll = Poll.new(poll_params)
+    if @poll.save
+      redirect_to @poll, notice: 'Poll was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def poll_params
+    params.require(:poll).permit(:vibe)
+  end
 end
