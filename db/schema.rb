1ea40ff437c63a592d0e4b27375a8c859751cdf4
AnamolThapa <anamolthapa7@gmail.com>
2024-08-26 14:07:05 +0900

    updated deadline

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2024_08_26_011921) do
-
-<!-- ActiveRecord::Schema[7.1].define(version: 2024_08_26_002322) -->
-    
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
