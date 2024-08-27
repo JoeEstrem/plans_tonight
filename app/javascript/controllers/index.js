d80009c65d9f68cc9f3b403e3a1771d6040f2cff
Igor Zapala <zapala.igor@gmail.com>
2024-08-27 11:53:57 +0900

    Confetti fires at each time we load the show page for plans

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,5 +2,5 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-import { Confetti } from "stimulus-confetti"
-application.register('confetti', Confetti)
+// import { Confetti } from "stimulus-confetti"
+// application.register('confetti', Confetti)
