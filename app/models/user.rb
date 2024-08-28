32cd816f5d0527d2cfecd0700c11306f2a404e5a
Igor Zapala <zapala.igor@gmail.com>
2024-08-28 18:15:28 +0900

    put index into partials, and fixed conditional for displaying

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :plans
   has_many :polls, dependent: :destroy
+  has_many :invited_plans, through: :polls, source: :plan
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :photo
